f8084e4e18385fffee8e47ba0bfd4eab7f060b8c
n12tt <navjotbamrah4@gmail.com>
2024-12-14 16:52:12 -0500

    Updates

---
+++ assets/with-props-BXkCMI4n.js
@@ -0,0 +1,1 @@
+import{v as t,w as e,x as o,y as p,r as s,z as u}from"./chunk-D52XG6IA-B2emolDZ.js";function i(a){return function(){const r={params:t(),loaderData:e(),actionData:o(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:t(),loaderData:e(),actionData:o(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
